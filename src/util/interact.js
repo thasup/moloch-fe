019034eeccaf83a4b62ae86f30c739facb25a95e
thasup <thanachonfirst@hotmail.com>
2022-09-28 09:21:16 +0700

    Create moloch contract

--- src/util/interact.js
+++ src/util/interact.js
@@ -1,3 +1,16 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x28cBFd8B8de17Aa41DBD19eC0bE567C568E1168C";
+
+export const molochDAOContract = new web3.eth.Contract(
+  contractABI,
+  contractAddress
+);
+
 export const loadCurrentMessage = async () => { 
   
 };
