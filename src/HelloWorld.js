9549135556bd2ffb0c823d8f42b2d3855f008340
thasup <thanachonfirst@hotmail.com>
2022-09-30 17:05:51 +0700

    Connecting metamask wallet

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,41 +1,96 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import {
-  helloWorldContract,
   connectWallet,
-  updateMessage,
-  loadCurrentMessage,
   getCurrentWalletConnected,
+  loadPeriodDuration,
+  loadVotingPeriodLength,
+  loadGracePeriodLength,
+  loadDilutionBound,
+  fetchMember,
+  loadTokenWhitelist,
+  loadProposalDeposit,
+  loadProcessingReward
 } from "./util/interact.js";
 
 import alchemylogo from "./alchemylogo.svg";
+import { async } from "q";
 
 const HelloWorld = () => {
   //state variables
   const [walletAddress, setWallet] = useState("");
   const [status, setStatus] = useState("");
-  const [message, setMessage] = useState("No connection to the network."); //default message
-  const [newMessage, setNewMessage] = useState("");
+  const [periodDuration, setPeriodDuration] = useState("");
+  const [votingPeriod, setVotingPeriod] = useState("");
+  const [gracePeriod, setGracePeriod] = useState("");
+  const [proposalDeposit, setProposalDeposit] = useState("");
+  const [dilutionBound, setDilutionBound] = useState("");
+  const [reward, setReward] = useState("");
+  const [inputTextValue, setInputTextValue] = useState("");
+  const [inputValue, setInputValue] = useState(null);
+  const [member, setMember] = useState({});
+  const [isWhitelist, setIsWhitelist] = useState(null);
 
   //called only once
-  useEffect(async () => {
-    
+  useEffect(() => {
+    async function fetchInitialParameters() {
+      setPeriodDuration(await loadPeriodDuration());
+      setVotingPeriod(await loadVotingPeriodLength());
+      setGracePeriod(await loadGracePeriodLength());
+      setProposalDeposit(await loadProposalDeposit());
+      setDilutionBound(await loadDilutionBound());
+      setReward(await loadProcessingReward());
+    }
+    fetchInitialParameters();
+
+    async function fetchWallet() {
+      const {address, status} = await getCurrentWalletConnected();
+      setWallet(address);
+      setStatus(status); 
+    }
+    fetchWallet();
+    addWalletListener();
   }, []);
 
-  function addSmartContractListener() { //TODO: implement
-    
+  async function onFetchMember(index) {
+    const memberData = await fetchMember(index);
+    setMember(memberData);
   }
 
-  function addWalletListener() { //TODO: implement
-    
+  async function onCheckWhitelist(text) {
+    const isValid = await loadTokenWhitelist(text);
+    setIsWhitelist(isValid);
   }
 
-  const connectWalletPressed = async () => { //TODO: implement
-    
-  };
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download`} rel="noreferrer">
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
-  const onUpdatePressed = async () => { //TODO: implement
-    
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
   //the UI of our component
@@ -45,32 +100,49 @@ const HelloWorld = () => {
       <button id="walletButton" onClick={connectWalletPressed}>
         {walletAddress.length > 0 ? (
           "Connected: " +
-          String(walletAddress).substring(0, 6) +
+          String(walletAddress).substring(0, 5) +
           "..." +
-          String(walletAddress).substring(38)
+          String(walletAddress).substring(walletAddress.length - 5)
         ) : (
           <span>Connect Wallet</span>
         )}
       </button>
 
-      <h2 style={{ paddingTop: "50px" }}>Current Message:</h2>
-      <p>{message}</p>
+      <h2 style={{ paddingTop: "18px" }}>Period Duration: {periodDuration}</h2>
 
-      <h2 style={{ paddingTop: "18px" }}>New Message:</h2>
+      <h2>Voting Period Length: {votingPeriod}</h2>
+      <h2>Grace Period Length: {gracePeriod}</h2>
+      <h2>Proposal Deposit: {proposalDeposit}</h2>
+      <h2>Dilution Bound: {dilutionBound}</h2>
+      <h2>Processing Reward: {reward}</h2>
 
-      <div>
+      {/* <div>
+        <h2 style={{ paddingTop: "18px" }}>Token Whitelist: {isWhitelist}</h2>
         <input
+          id="fetchAddress"
           type="text"
-          placeholder="Update the message in your smart contract."
-          onChange={(e) => setNewMessage(e.target.value)}
-          value={newMessage}
+          placeholder="Insert the address"
+          onChange={(e) => setInputTextValue(e.target.value)}
+          // onChange= {(e) => onCheckWhitelist(e.value.target)}
         />
-        <p id="status">{status}</p>
-
-        <button id="publish" onClick={onUpdatePressed}>
-          Update
+        <button id="fetchAddress" onClick={onCheckWhitelist(inputTextValue)}>
+          Fetch
         </button>
       </div>
+
+      <div>
+        <h2 style={{ paddingTop: "18px" }}>Member: {member}</h2>
+        <input
+          id="fetchMember"
+          type="text"
+          placeholder="Insert the index"
+          onChange={(e) => setInputValue(e.target.value)}
+          // onChange= {(e) => onCheckWhitelist(e.value.target)}
+        />
+        <button id="fetchMember" onClick={onFetchMember(inputValue)}>
+          Fetch
+        </button>
+      </div> */}
     </div>
   );
 };
--- src/util/interact.js
+++ src/util/interact.js
@@ -11,18 +11,112 @@ export const molochDAOContract = new web3.eth.Contract(
   contractAddress
 );
 
-export const loadCurrentMessage = async () => { 
-  
+export const loadPeriodDuration = async () => { 
+  return await molochDAOContract.methods.periodDuration().call(); 
+};
+
+export const loadVotingPeriodLength = async () => { 
+  return await molochDAOContract.methods.votingPeriodLength().call();
+};
+
+export const loadGracePeriodLength = async () => { 
+  return await molochDAOContract.methods.gracePeriodLength().call();
+};
+
+export const loadProposalDeposit = async () => { 
+  return await molochDAOContract.methods.proposalDeposit().call();
+};
+
+export const loadDilutionBound = async () => { 
+  return await molochDAOContract.methods.dilutionBound().call();
+};
+
+export const loadProcessingReward = async () => { 
+  return await molochDAOContract.methods.processingReward().call();
+};
+
+export const fetchMember = async (index) => { 
+  return await molochDAOContract.methods.members(index).call();
+};
+
+export const loadTokenWhitelist = async (address) => { 
+  return await molochDAOContract.methods.tokenWhitelist(address).call();
 };
 
 export const connectWallet = async () => {
-  
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        status: "👆🏽 Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download`} rel="noreferrer">
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
 };
 
 export const getCurrentWalletConnected = async () => {
-  
-};
-
-export const updateMessage = async (address, message) => {
-  
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: "👆🏽 Write a message in the text-field above.",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button.",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank" href={`https://metamask.io/download`} rel="noreferrer">
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
 };
